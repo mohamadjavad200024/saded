7a97a6d8abc6974580525283b128205471d4dbb7
MohamadJavad22 <mohamadjavad221403@gmail.com>
2025-12-06 15:08:23 +0400

    Update ecosystem.config.js with correct paths and port 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,12 +15,13 @@ module.exports = {
       name: 'saded',
       script: 'node_modules/.bin/next',
       args: 'start',
-      cwd: './',
+      cwd: '/home/shop1111/repositories/saded',
       instances: 1,
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000,
+        PORT: 3001,
+        NODE_OPTIONS: '--max-old-space-size=4096'
       },
       error_file: './logs/pm2-error.log',
       out_file: './logs/pm2-out.log',
