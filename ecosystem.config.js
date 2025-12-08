21c95e07af9ab15b1e114b964cb0a55184c22994
MohamadJavad22 <mohamadjavad221403@gmail.com>
2025-12-08 19:16:02 +0400

    Disable WebAssembly in NODE_OPTIONS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 3001,
         HOSTNAME: '0.0.0.0',
-        NODE_OPTIONS: '--max-old-space-size=2048',
+        NODE_OPTIONS: '--max-old-space-size=2048 --no-wasm',
         PATH: '/opt/alt/alt-nodejs20/root/usr/bin:/usr/local/bin:/usr/bin:/bin',
         NODE_PATH: '/opt/alt/alt-nodejs20/root/usr/lib/node_modules',
         DB_HOST: 'localhost',
