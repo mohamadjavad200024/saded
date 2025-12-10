80b3f78b071edefcb5a89aa02160f4a984b3f054
MohamadJavad22 <mohamadjavad221403@gmail.com>
2025-12-10 18:01:19 +0400

    Rebuild: After adding missing pages (about, contact, faq, shipping, returns, warranty, blog)

--- .next/server/app/_not-found/page.js
+++ .next/server/app/_not-found/page.js
@@ -2,7 +2,7 @@ var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_not-found/
 R.c("server/chunks/ssr/[root-of-the-server]__296a25b5._.js")
 R.c("server/chunks/ssr/_d25179f9._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_esm_build_templates_app-page_b8e1111a.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0b4cb87a._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__a9a45fcb._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_30e9ea0b._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_982e7503._.js")
 R.c("server/chunks/ssr/app_b9b1292a._.js")
--- .next/server/app/sitemap.xml/route.js
+++ .next/server/app/sitemap.xml/route.js
@@ -1,5 +1,5 @@
 var R=require("../../chunks/[turbopack]_runtime.js")("server/app/sitemap.xml/route.js")
-R.c("server/chunks/[root-of-the-server]__d345c0e7._.js")
+R.c("server/chunks/[root-of-the-server]__11aefb3f._.js")
 R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
 R.c("server/chunks/_next-internal_server_app_sitemap_xml_route_actions_12658ace.js")
 R.m(8922)
