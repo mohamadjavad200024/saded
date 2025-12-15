a1e8bb95a3975228c31120966b4e30af48e7c297
MohamadJavad22 <mohamadjavad221403@gmail.com>
2025-12-15 23:42:03 +0400

    feat: اضافه کردن فایلهای build شده برای سیستم مدیریت محتوای Footer

---
+++ .next/server/app/api/settings/site-content/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/settings/site-content/route.js")
+R.c("server/chunks/[root-of-the-server]__fd55258d._.js")
+R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
+R.c("server/chunks/_next-internal_server_app_api_settings_site-content_route_actions_abc0887c.js")
+R.m(64282)
+module.exports=R.m(64282).exports
---
+++ .next/server/chunks/_next-internal_server_app_api_settings_site-content_route_actions_abc0887c.js
@@ -0,0 +1,3 @@
+module.exports=[99291,(e,o,d)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_api_settings_site-content_route_actions_abc0887c.js.map
